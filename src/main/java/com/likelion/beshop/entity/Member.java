7a4eab6a4246617fd2143bcd528d006e91059615
jhsseonn <hs125bt@gmail.com>
2023-03-19 20:43:59 +0900

    feat: Member 클래스, dto 생성 및 repository 생성 - 엔티티 설계

--- src/main/java/com/likelion/beshop/entity/Member.java
+++ src/main/java/com/likelion/beshop/entity/Member.java
@@ -1,4 +1,43 @@
 package com.likelion.beshop.entity;
 
+import com.likelion.beshop.constant.Role;
+import com.likelion.beshop.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="member")
+@Getter @Setter
+@ToString
 public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+//    @Enumerated(EnumType.STRING)
+//    private Role role;
+
+    public static Member createMember(MemberFormDto memberFormDto) {
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        member.setPassword(memberFormDto.getPassword());
+//        member.setRole(Role.USER);
+        return member;
+    }
 }
